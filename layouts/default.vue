d405fb58f83c221b26e6e36205938f1ea629c946
y2kzest <aaron.crp28@gmail.com>
2025-06-20 10:36:11 +0800

    added login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -104,8 +104,8 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Sign In',
+          to: '/signin'
         }
       ],
       miniVariant: false,
---
+++ pages/signin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex mx-auto justify-center align-center" style="height: 100vh;">
+        <v-card max-width="700">
+            <v-card-text>
+                <h1 style="text-align: center; color: white;">Login</h1>
+                <v-text-field label="Email" prepend-icon="mdi-email"></v-text-field>
+                <v-text-field label="Password" prepend-icon="mdi-lock" type="password"></v-text-field>
+                <v-btn 
+                color="primary"
+                elevation="2">Sign In</v-btn>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script>
+export default{
+    layout: "auth",
+}
+</script>
